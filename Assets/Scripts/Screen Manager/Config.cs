3c5d18b74dced6c156d05bf8583e1755e7b4e112
DeamusPinkerton <102838103+DeamusPinkerton@users.noreply.github.com>
2022-12-02 23:44:09 -0300

    ScreenManager(2)

--- Assets/Scripts/Screen Manager/Config.cs
+++ Assets/Scripts/Screen Manager/Config.cs
@@ -13,36 +13,11 @@ public class Config : MonoBehaviour
     {
         ScreenManager.Instance.Push(new ScreenGO(_mainGame));
     }
-    /*
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            var res = Instantiate(_overlayGame);
 
-            ScreenManager.Instance.Push(new ScreenGO(res));
-        }
-        else if (Input.GetKeyDown(KeyCode.P))
-        {
-            
-            var screenOptions = Instantiate(Resources.Load<ScreenOptions>("Options Screen"), _canvasFather);
-            ScreenManager.Instance.Push(screenOptions);
-        }
-        else if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            ScreenManager.Instance.Pop();
-        }
-    }
-    */
     public void OptionsMenu()
     {
-        //ScreenManager.Instance.Push("Options Screen", _canvasFather);
-        /*
-        var screenOptions = Instantiate(Resources.Load<ScreenOptions>("OptionsMenu"), _canvasFather);
-        ScreenManager.Instance.Push(screenOptions);
-        */
-        var screenOptions = Instantiate(Resources.Load<SettingsMenu>("Options Screen"), _canvasFather);
-        ScreenManager.Instance.Push(screenOptions);
+        var ScreenOptions = Instantiate(Resources.Load<ScreenOptions>("SettingsMenu"), _canvasFather);
+        ScreenManager.Instance.Push(ScreenOptions);
     }
     public void MenuBack()
     {
